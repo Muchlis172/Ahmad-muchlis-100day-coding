b06b9e7c75823937d1c80551eb8ca8665a2c206e
Muchlis172 <nurhidaya9631@gmail.com>
2024-09-03 18:41:35 +0800

    Add files via upload
    Day2

---
+++ day2.java
@@ -0,0 +1,10 @@
+
+public class day2 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, senior!");
+        System.out.println("pada hari senin aku kecelakaan");
+        System.out.println("hari selasa aku baru mulai ngerja tugas");
+        System.out.println("tugas nya adalah ngoding 100day");}
+    
+}
