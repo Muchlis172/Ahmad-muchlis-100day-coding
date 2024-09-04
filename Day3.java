3f86c565ee704022801ab7e805a0b733d663df9e
Muchlis172 <nurhidaya9631@gmail.com>
2024-09-04 15:42:49 +0800

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,12 @@
+package Coding100day;
+
+public class Day3 {
+    
+    public static void main(String[] args) {
+        System.out.println("Ahmad muchlis");
+        System.out.println("NIM:D0224335");
+        System.out.println("Prodi Infomatika");
+        System.out.println("Class D");
+    }
+    
+}
