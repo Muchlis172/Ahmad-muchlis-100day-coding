2d5eef6322826df97a7a2440e94090178fac3057
Muchlis172 <nurhidaya9631@gmail.com>
2024-09-05 22:18:30 +0800

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,17 @@
+package ngoding100day;
+    
+public class day4 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Nama \t\t: Ahmad muchlis");
+        System.out.println("Prodi \t\t: informatika");
+        System.out.println("Class \t\t: D");
+        // backslash merapikan titik dua
+        
+        System.out.printf("Nilai rata-rata : %d",87);
+        
+        
+    }
+    
+}
