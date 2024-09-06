e7818fabfb57d73b9cd512326418b397d11a7bd5
Muchlis172 <nurhidaya9631@gmail.com>
2024-09-06 14:56:35 +0800

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,12 @@
+package coding100day;
+
+public class day5 {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama \t\t: Ahmad muchlis");
+   //     System.out.println("Prodi \t\t: informatika"); 
+   //     System.out.println("Class \t\t: D");
+        System.out.printf("Nilai rata-rata : %d",87);
+    }
+    
+}
